28f74e3573381df589a536b4a3dbd2766b12ed80
Max Edgington <edgington.m.w@gmail.com>
2023-11-07 12:24:53 -0500

    displaying product description

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  // options object - add optional properties to config app
+
+  // data option
+  // this then gets imported into html and mounted into dom
+  // phone from html to js with double curly bracket for js expressions - valid js in html
+
+  data: function () {
+    return {
+      product: "Boots",
+      description: "Workwear for your everyday needs",
+    };
+  },
+});
